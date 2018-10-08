d524c1a8115d7ff0bcdab034e44a2f1cf593ad52
SehwaKim <sehwakm@gmail.com>
2018-10-08 11:43:34 +0900

    프로젝트 생성

--- chatClient/src/java/ChatClientMain.java
+++ chatClient/src/java/ChatClientMain.java
@@ -1,4 +1,4 @@
-public class ChatServer {
+public class ChatClientMain {
     public static void main(String[] args) {
 
     }
---
+++ chatServer/src/java/ChatServerMain.java
@@ -0,0 +1,5 @@
+public class ChatServerMain {
+    public static void main(String[] args) {
+
+    }
+}
